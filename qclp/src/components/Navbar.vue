90658c2bffe255066b687c6c4ed5d398d1a89f4c
jayhjj <2762319789@qq.com>
2020-04-08 10:57:17 +0800

    Navbar

--- qclp/src/components/Navbar.vue
+++ qclp/src/components/Navbar.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="head">
-    <el-row :gutter="20">
-      <el-col :span="8">qclp</el-col>
-      <el-col :span="16">
+      <div class="header">
+        <el-row  type="flex" class="row-bg" justify="space-around">
+      <!-- <el-col :span="8">qclp</el-col> -->
+      <el-col :span="6">
         <el-menu 
           :default-active="activeIndex"
           class="el-menu-demo"
           mode="horizontal"
           @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
           >
           <div v-for="(item,index) in tabcontent" :key="index" style="display:inline-block;">
              <el-submenu v-if="item.children" :index="item.id">
@@ -22,15 +26,39 @@
           </div>
         </el-menu>
       </el-col>
+      <el-col :span="6" >qclp</el-col>
+      <el-col :span="6" >
+        <el-menu 
+          :default-active="activeIndex1"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+         
+          >
+          <div v-for="(item,index) in tabcontent1" :key="index" style="display:inline-block;">
+             <el-submenu v-if="item.children" :index="item.id">
+              <template slot="title">{{item.text}}</template>
+              <el-menu-item v-for="(sub,indey) in item.children" :key="indey" :index="sub.id">
+                {{sub.text}}
+              </el-menu-item>
+             </el-submenu>
+              <el-menu-item v-else :index="item.id">
+                {{item.text}}
+              </el-menu-item>
+          </div>
+        </el-menu>
+      </el-col>
     </el-row>
+      </div>
   </div>
 </template>
 
 <script>
-
 export default {
   components: {
-    
   },
   data() {
     return {
@@ -112,7 +140,11 @@ export default {
             {
                 id:'4',
                 text:'餐厅查询'
-            },
+            }   
+        ],
+        
+         activeIndex1: '1',
+         tabcontent1:[
             {
                 id:'5',
                 text:'加盟合作',
@@ -194,13 +226,42 @@ export default {
 
 <style lang="stylus" scoped>
 .head
+    margin 0
+    padding 0
   width 100%
-  // display flex
-  background-color #fff
-  height 60px
-  line-height 60px
+//   display flex
+//   background-color blue
+  height 140px
+  line-height 100px
   justify-content space-between
   box-shadow: 0 3px 3px rgba(100, 100, 100, .3)
- 
-</style>
+//   box-shadow: 0 10px 10px rgba(0, 0, 0, .12), 0 0 16px rgba(0, 0, 0, .10)
+// .header
+//     width 100%
+//      height 140px
+//      line-height 100px
+     
+.el-menu.el-menu--horizontal {
+    border-bottom: none ;
+    // box-shadow: 0 10px 10px rgba(0, 0, 0, .12), 0 0 16px rgba(0, 0, 0, .10)
+}
+div 
+    font-size 40px
+.row-bg 
+    // padding: 10px 0;
+    background-color:#545c64;
+    // text-color:#fff;
+    // active-text-color:#ffd04b;
+    box-shadow: 0 20px 20px rgba(0, 0, 0, .16), 0 0 20px rgba(0, 0, 0, .16)
+    height 140px
+.el-menu-demo
+    // background-color:blue;
+  
+    padding-top 60px
+.el-menu-item
+    // font-size 18px
+    // color #fff
+.title
+    font-size 18px
 
+</style>
