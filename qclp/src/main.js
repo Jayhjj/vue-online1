41fa927a7eda904da55d22598ca69e54e02d1af2
1939108122 <1939108122@qq.com>
2020-04-08 17:51:53 +0800

    swiper finished

--- qclp/src/main.js
+++ qclp/src/main.js
@@ -4,6 +4,10 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+import 'swiper/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
